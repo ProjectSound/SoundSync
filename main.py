51fa87bfb14048e2c55e2bedcb420c83121cba0c
AkatLamaz <dmnkgizaa@gmail.com>
2023-11-10 20:30:43 +0100

    Added Threshold edit for inputs

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ class AudioController:
                 interface.SetMasterVolume(self.volume, None)
                 print("Volume set to", self.volume)  # debug
 
+    @staticmethod
     def list_applications() -> list:
         sessions = AudioUtilities.GetAllSessions()
         apps = []
@@ -54,100 +55,83 @@ class AudioController:
             if session.Process:
                 print(f"Index {index}: {session.Process.name()}")
                 apps.append(session.Process.name())
-        return apps        
-
+        return apps
+    
 apps = AudioController.list_applications()
 print("Enter index of app")
 app_index = int(input())
+if app_index >= len(apps):
+    print("Invalid index. Please enter a number between 0 and", len(apps)-1)
+else:
+    audio_controller = AudioController(apps[app_index])
+
+threshold = []
+threshold_not_reached = []
+
+for i in range(2):
+    print(f"Enter the threshold value for input {i+1}:")
+    threshold.append(float(input()))
+
+    print(f"Enter the threshold not reached duration (in seconds) for input {i+1}:")
+    threshold_not_reached.append(int(input()))
 
 max_rms = 0.1
-threshold = 5
-timeout_thread = None
-timeout_timestamp = None
+timeout_threads = [None] * len(threshold)
 audio_controller = AudioController(apps[app_index])
 max_amplitude = 2**24 - 1  # Dla dźwięku o rozdzielczości 16 bitów
 
 rms_values = collections.deque(maxlen=10)
 max_rms_values = collections.deque(maxlen=1000)
 
-def audio_callback(indata, frames: int, time: float, status: sd.CallbackFlags) -> None:
-    global max_rms
-    global threshold
-    global timeout_thread
-    global timeout_timestamp
+#create_audio_callback
+def create_audio_callback(threshold, threshold_not_reached):
+    timeout_threads = [None] * len(threshold)
+    timeout_timestamps = [None] * len(threshold)
+    max_rms = 0.1
 
-    mapped_value = np.linalg.norm(indata)*10
+    def audio_callback(indata, frames: int, time: float, status: sd.CallbackFlags) -> None:
+        nonlocal timeout_threads
+        nonlocal timeout_timestamps
+        nonlocal max_rms
+            # global threshold
+            # global timeout_thread
+            # global timeout_timestamp
 
-    rms = np.linalg.norm(indata)  # Calculate RMS value
-    rms_values.append(rms)  # Add RMS value to deque
-    avg_rms = sum(rms_values) / len(rms_values)  # Calculate moving average of RMS values
+        rms = np.linalg.norm(indata)
+        min_rms = 0.1
+        max_rms = max(max_rms, rms)
+        if (max_rms - min_rms) > 0:
+            mapped_value = (rms - min_rms) / (max_rms - min_rms) * 100.0
+            mapped_value = min(max(mapped_value, 0), 100)
+        else:
+            mapped_value = 0
 
-    min_rms = 0.1  # Minimum RMS value to 0%
-    max_rms = max(max_rms, avg_rms)  # Update maximum RMS value
+        for i in range(len(threshold)):
+            if mapped_value >= threshold[i] and timeout_threads[i] is not None:
+                timeout_timestamps[i] = datetime.now()
 
-    if (max_rms - min_rms) > 0:
-        mapped_value = (avg_rms - min_rms) / (max_rms - min_rms) * 100.0  # Scale value from 0% to 100%
-    else:
-        mapped_value = 0
+            if mapped_value >= threshold[i] and timeout_threads[i] is None:
+                print(f"Próg głośności dla input {i+1} osiągnięty, rozpoczynam wątek timeouta")
+                fade_audio(1.0, 0.05, 1)
+                timeout_threads[i] = threading.Thread(target=timeout_handler, args=(i,))
+                timeout_threads[i].start()
 
-    compression_ratio = 2.0
-    threshold = 50.0
-    if mapped_value > threshold:
-        mapped_value = threshold + (mapped_value - threshold) / compression_ratio
+            elif mapped_value < threshold[i] and timeout_threads[i] is not None:
+                if timeout_timestamps[i] is not None:
+                    time_elapsed = datetime.now() - timeout_timestamps[i]
+                    if time_elapsed >= timedelta(seconds=threshold_not_reached[i]):
+                        print(f"Poziom głośności dla input {i+1} poniżej progu przez {threshold_not_reached[i]} sekund, kończę wątek timeouta")
+                        timeout_threads[i].join()
+                        print("Zwiększam poziom")
+                        fade_audio(0.05, 1.0, 1)
+                        timeout_threads[i] = None
+        sd.sleep(1)
 
-    # rms = np.linalg.norm(indata)  # Oblicz wartość RMS sygnału
-    # min_rms = 0.1  # Minimalna wartość RMS, która odpowiada 0%
-    # max_rms = max(max_rms, rms)  # Zaktualizuj maksymalną wartość RMS
-    # if (max_rms - min_rms) > 0:
-    #     mapped_value = (rms - min_rms) / (max_rms - min_rms) * 100.0  # Skaluj wartość od 0% do 100%
-    # else:
-    #     mapped_value = 0
-
-    # print(rms)
-
-    #print(f"Poziom głośności: {mapped_value:.2f}% (Maks RMS: {max_rms:.2f})")
-
-    if mapped_value >= threshold and timeout_thread is not None:
-        timeout_timestamp = datetime.now()  # Zapisz czas przekroczenia progu
-
-    if mapped_value >= threshold and timeout_thread is None:
-        print("Próg głośności osiągnięty, rozpoczynam wątek timeouta")
-        fade_audio(1.0, 0.05, 1)
-        # audio_controller.set_volume(0.05)
-        timeout_thread = threading.Thread(target=timeout_handler)
-        timeout_thread.start()
-
-    elif mapped_value < threshold and timeout_thread is not None:
-        if timeout_timestamp is not None:
-            time_elapsed = datetime.now() - timeout_timestamp
-            if time_elapsed >= timedelta(seconds=5):
-                print("Poziom głośności poniżej progu przez 5 sekund, kończę wątek timeouta")
-                timeout_thread.join()
-                print("Zwiększam poziom")
-                fade_audio(0.05, 1.0, 1)
-                # audio_controller.set_volume(1.0)
-                timeout_thread = None
-    
-    #print(max(lisa))
-    sd.sleep(1)
-
+    return audio_callback        
 
 devices = sd.query_devices()
 input_devices = [device for device in devices if device['max_input_channels'] > 0 and not device['name'].startswith('Loopback')]
 device_names = set()
-
-#show all with channels
-# for device in devices:
-#     if device['max_input_channels'] > 0 and not device['name'].startswith('Loopback'):
-#         device_name = device['name']
-#         if device_name not in device_names:
-#             input_devices.append(device)
-#             device_names.add(device_name)
-
-# print("Available input devices:")
-# for i, device in enumerate(input_devices):
-#     print(f"Device {i}: {device['name']} - {device['hostapi']} - {device['max_input_channels']} channels")
-#show all without channels
 
 devices = sd.query_devices()
 for i, device in enumerate(devices):
@@ -159,21 +143,19 @@ for i in range(2):
     device_index = int(input(f"Enter the index of input device {i+1}: "))
     device_indices.append(device_index)
 
-    # Create input streams for the chosen devices
+# Create input streams for the chosen devices
 streams = []
+threads = []
+
 for i in range(2):
     device_name = input_devices[device_indices[i]]['name']
-    stream = sd.InputStream(device=device_name, channels=2, callback=audio_callback)
+    stream = sd.InputStream(device=device_name, channels=2, callback=create_audio_callback(threshold=[threshold[i]], threshold_not_reached=[threshold_not_reached[i]]))
     streams.append(stream)
 
     # Start the input streams in separate threads
-threads = []
-for i in range(2):
-    thread = threading.Thread(target=streams[i].start)
+    thread = threading.Thread(target=stream.start)
     threads.append(thread)
-    threads[i].start()
-
-
+    thread.start()
 
 def fade_audio(start: float, end: float, duration: int = 2) -> None:
     """
@@ -204,13 +186,11 @@ def fade_audio(start: float, end: float, duration: int = 2) -> None:
             time_to_sleep = 0
         else:
             time_to_sleep = (duration - elapsed_time) / (abs(end - value))  # Oblicz czas oczekiwania na kolejną iterację
-
         if time_to_sleep > 0:
             time.sleep(time_to_sleep)
 
-
-def timeout_handler() -> None:
-    time.sleep(5)  # Oczekuj 5 sekund
+def timeout_handler(i) -> None:
+    time.sleep(threshold_not_reached[i])  # Oczekuj threshold dla input1 oraz input2 sekund
 
 if __name__ == "__main__":
     try:
